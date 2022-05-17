5f5c13ca1a34df24931d61e27382a17b8603d83e
SushiQ <dnordm@kth.se>
2022-05-17 11:46:41 +0200

    Create brotoss.py

---
+++ examples/protoss/brotoss.py
@@ -0,0 +1,43 @@
+import random
+
+from sc2 import maps
+from sc2.bot_ai import BotAI
+from sc2.data import Difficulty, Race
+from sc2.ids.unit_typeid import UnitTypeId
+from sc2.main import run_game
+from sc2.player import Bot, Computer
+
+
+class broBot(BotAI):
+
+    # pylint: disable=R0912
+    async def on_step(self, iteration):
+        if iteration == 0:
+            await self.chat_send("(probe)(pylon)(cannon)(cannon)(gg)")
+
+        if not self.townhalls:
+            # Attack with all workers if we don't have any nexuses left, attack-move on enemy spawn (doesn't work on 4 player map) so that probes auto attack on the way
+            for worker in self.workers:
+                worker.attack(self.enemy_start_locations[0])
+            return
+
+        nexus = self.townhalls.random
+
+        # Make probes until we have 16 total
+        if self.supply_workers < 16 and nexus.is_idle:
+            if self.can_afford(UnitTypeId.PROBE):
+                nexus.train(UnitTypeId.PROBE)
+
+
+
+def main():
+    run_game(
+        maps.get("BerlingradAIE"),
+        [Bot(Race.Protoss, CannonRushBot(), name="CheeseCannon"),
+         Computer(Race.Protoss, Difficulty.Medium)],
+        realtime=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
