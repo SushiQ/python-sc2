7d4ec5150099f2f97b3ccb928867d8e642df79ec
SushiQ <dnordm@kth.se>
2022-05-25 17:40:53 +0200

    Implemented BT into rush_base.py
    (Still not working though)

--- examples/behaviour_tree.py
+++ examples/behaviour_tree.py
@@ -1,16 +1,11 @@
 from datetime import datetime
 from abc import ABC, abstractmethod
 from enum import Enum, auto
-
-#get current date
-dt = datetime.today()
-seconds = round(dt.timestamp())
-print(seconds)
-
-evaluate_time = True
+from time import sleep
 
 
-
+global seconds
+seconds = 6
 
 class FastBaseBot():
     def __init__(self, clock):
@@ -24,9 +19,14 @@ class State(Enum):
 
 
 class BTNode(FastBaseBot):
-    _nodeState = State("_nodeState")
-    def nodeState():
-        return _nodeState
+    def __init__(self):
+        self._nodeState = State.FAILURE
+
+    def set_state(self, state):
+        self._nodeState = state
+
+    def get_state(self):
+        return self._nodeState
 
     @abstractmethod
     def Evaluate(self):
@@ -34,14 +34,20 @@ class BTNode(FastBaseBot):
 
 
 class Selector(BTNode):
-    nodes = []
+    print("IN Selector")
+
+    def __init__(self, nodes):
+        self.node_list = nodes;
 
     def Selector(node_list):
         self.node_list = node_list;
 
-
     def Evaluate():
-        for node in nodes:
+        print("IN Selector -> Evaluate")
+
+        print("node_list = ", self.node_list)
+
+        for node in self.node_list:
             if node.Evaluate() == State.RUNNING:
                 _nodeState = State.RUNNING
                 return _nodeState
@@ -58,28 +64,41 @@ class Selector(BTNode):
 
 
 class Sequence(BTNode):
-    nodes = []
+    print("IN Sequence")
+    def __init__(self, nodes):
+        self.node_list = nodes;
 
-    def Selector(node_list):
+    def Sequence(node_list):
         self.node_list = node_list;
 
-    def Evaluate():
-        isAnyNodeRunning = false
+    def Evaluate(self):
+        print("IN Sequence -> Evaluate")
 
-        for node in nodes:
-            if node.Evaluate() == State.RUNNING:
-                isAnyNodeRunning = true
-                break
-            elif node.Evaluate() == State.SUCCESS:
-                break
-            elif node.Evaluate() == State.FAILURE:
-                _nodeState = State.FAILURE;
-                break
+        isAnyNodeRunning = False
+        print("node_list = ", self.node_list)
+
+        #for node in self.node_list:
+        i = 0
+
+        while i < len(self.node_list):
+            node = self.node_list[i]
+            print("i = ", i, " and node = ", node)
+            node._nodeState = node.Evaluate()
+            print("node._nodeState = ", node._nodeState)
+
+            if node._nodeState == State.SUCCESS:
+                print("!! Evaluate next node")
+                self.node_list[i].Evaluate()
+                i += 1
+                continue
+
             else:
-                break
+                print("?? Evaluate this node")
 
-            _nodeState = State.FAILURE
-            return _nodeState
+                node._nodeState = node.Evaluate()
+
+            #_nodeState = State.FAILURE
+            #return _nodeState
 
         if isAnyNodeRunning:
             _nodeState = State.RUNNING
@@ -89,16 +108,66 @@ class Sequence(BTNode):
         return _nodeState
 
 
+class twoSecond(BTNode):
+    print("twoSecond! ")
+
+    def Evaluate(self):
+        print("twoSecond -> Evaluate! ")
+
+        if seconds % 2 == 0:
+            print("self._nodeState Before: ", self._nodeState)
+
+            print("1. seconds= ", seconds)
+
+            print("1. True!")
+            self._nodeState = State.SUCCESS
+            print("self._nodeState After: ", self._nodeState)
+            return State.SUCCESS
+
+        else:
+            print("1. seconds = ", seconds)
+
+            print("1. False!")
+            self._nodeState = State.FAILURE
+            return State.FAILURE
+
+
+class threeSecond(BTNode):
+    def Evaluate(self):
+
+        if seconds % 3 == 0:
+            print("2. Also True!")
+            self._nodeState = State.SUCCESS
+            return State.SUCCESS
+        else:
+            print("2. False!")
+            self._nodeState = State.FAILURE
+            return State.FAILURE
+
+
+class fourSecond(BTNode):
+    def Evaluate(self):
+
+        if seconds % 4 == 0:
+            print("3. ALSO ALSO True!")
+            self._nodeState = State.SUCCESS
+            return State.SUCCESS
+        else:
+            print("3. False!")
+            self._nodeState = State.FAILURE
+            return State.FAILURE
+
 
 class BehaviorTree():
     checkTwoSecond = twoSecond()
-    checkThreeSecond = BTNode()
-    self.checkTwoSecond = checkTwoSecond
+    #checkTwoSecond.Evaluate()
 
+    checkThreeSecond = threeSecond()
+    #checkThreeSecond.Evaluate()
 
+    checkFourSecond = fourSecond()
 
-    clockSequence = [ checkTwoSecond, checkThreeSecond]
-    clock = clockSequence
-
-    while evaluate_time():
-        self.clock.Evaluate()
+    #clockSequence = BTNode()
+    clockSequence = Sequence( [checkTwoSecond, checkThreeSecond, checkFourSecond] )
+    #print([checkTwoSecond.Evaluate(), checkThreeSecond.Evaluate()])
+    clockSequence.Evaluate()
--- examples/protoss/rush_base.py
+++ examples/protoss/rush_base.py
@@ -7,10 +7,19 @@ from sc2.ids.unit_typeid import UnitTypeId
 from sc2.main import run_game
 from sc2.player import Bot, Computer
 
-import py_trees
-import random
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+from time import sleep
+
+class State(Enum):
+    RUNNING = auto()
+    SUCCESS = auto()
+    FAILURE = auto()
 
 class FastBaseBot(BotAI):
+    def __init__(self):
+        army = BTNode()
+        self.army = army
 
     # pylint: disable=R0912
     async def on_step(self, iteration):
@@ -76,25 +85,149 @@ class FastBaseBot(BotAI):
                         worker.build_gas(vg)
                         worker.stop(queue=True)
 
-        # Build GATEWAY and CYBERNETICSCORE when pylon is built
-        if self.structures(UnitTypeId.PYLON).ready + self.already_pending(UnitTypeId.NEXUS) > 1 + self.townhalls.ready.amount = :
-            proxy = self.structures(UnitTypeId.PYLON).closest_to(self.enemy_start_locations[0])
-            pylon = self.structures(UnitTypeId.PYLON).ready.random
-            if self.structures(UnitTypeId.GATEWAY).ready:
-                # If we have no cyber core, build one
-                if not self.structures(UnitTypeId.CYBERNETICSCORE):
-                    if (
-                        self.can_afford(UnitTypeId.CYBERNETICSCORE)
-                        and self.already_pending(UnitTypeId.CYBERNETICSCORE) == 0
-                    ):
-                        await self.build(UnitTypeId.CYBERNETICSCORE, near=pylon)
-            # Build up to 2 gates
-            if (
-                self.can_afford(UnitTypeId.GATEWAY)
-                and self.structures(UnitTypeId.WARPGATE).amount + self.structures(UnitTypeId.GATEWAY).amount < 2
-            ):
-                await self.build(UnitTypeId.GATEWAY, near=pylon)
 
+
+
+
+
+        class BehaviorTree():
+            scout = scoutMap(self.game_info, self.units())
+
+            kite = kiteEnemy(self.game_info, self.units())
+
+            attack = attackEnemy(self.game_info, self.units())
+
+            #flee = fleeEnemy()
+
+
+            armySequence = Sequence( [scout, kite, attack] )
+            #print([checkTwoSecond.Evaluate(), checkThreeSecond.Evaluate()])
+            armySequence.Evaluate()
+
+
+class BTNode(FastBaseBot):
+    def __init__(self):
+        self._nodeState = State.FAILURE
+
+    def set_state(self, state):
+        self._nodeState = state
+
+    def get_state(self):
+        return self._nodeState
+
+    @abstractmethod
+    def Evaluate(self):
+        pass
+
+
+class Sequence(BTNode):
+    print("IN Sequence")
+    def __init__(self, nodes):
+        self.node_list = nodes;
+
+    def Sequence(node_list):
+        self.node_list = node_list;
+
+    def Evaluate(self):
+        isAnyNodeRunning = False
+        #for node in self.node_list:
+        i = 0
+        while i < len(self.node_list):
+            node = self.node_list[i]
+            node._nodeState = node.Evaluate()
+
+            # Evaluate next node/behaviour if first behaviour is SUCCESS
+            if node._nodeState == State.SUCCESS:
+                self.node_list[i].Evaluate()
+                i += 1
+                continue
+            # Continue evaluating this behaviour otherwise
+            else:
+                node._nodeState = node.Evaluate()
+
+        if isAnyNodeRunning:
+            _nodeState = State.RUNNING
+        else:
+            _nodeState = State.SUCCESS
+
+        return _nodeState
+
+
+
+class scoutMap(BTNode):
+    def __init__(self, game_info, units):
+        self._game_info = game_info
+        self.units = units
+
+    # I want to return the unit who entered our vision together with a boolean
+    def on_enemy_unit_entered_vision(self, unit: Unit): ### This does not work
+        return True, self.unit
+
+    def Evaluate(self):
+        if self.units().amount > 1:
+            for unit in self.units():
+                unit.move(self._game_info.map_center)
+
+        seen_enemy, unit = self.on_enemy_unit_entered_vision()
+        if seen_enemy:
+            self._nodeState = State.SUCCESS
+            return State.SUCCESS
+
+        self._nodeState = State.FAILURE
+        return State.FAILURE
+
+
+class kiteEnemy(BTNode):
+    def __init__(self, game_info, units):
+        self._game_info = game_info
+        self.units = units
+
+    def on_enemy_unit_entered_vision(self, unit: Unit):
+        return True, unit
+
+    def Evaluate(self):
+        # Find the closest enemy
+        enemyUnits = self.enemy_units.closer_than(5, r) # hardcoded attack range of 5
+        enemyUnits = enemyUnits.sorted(lambda x: x.distance_to(r))
+        closestEnemy = enemyUnits[0]
+
+        seen_enemy, unit = self.on_enemy_unit_entered_vision()
+        # If the enemy is a ranged unit, we cannot KITE -> Go to next behaviour in the Sequence
+        if unit(UnitTypeId.VOIDRAY) or unit(UnitTypeId.STALKER):
+            self._nodeState = State.SUCCESS
+            return State.SUCCESS
+        # Else: Keep distance and continue to attack
+        else:
+            for unit in self.units():
+                # If we have stalkers, use them to KITE
+                if unit(UnitTypeId.STALKER):
+                    if _distance_squared_unit_to_unit_method0(unit,enemy) < 20:
+                        # Move away from enemy
+                        unit.move(self._game_info.map_center.towards(self._game_info.start_location, random.randrange(8, 15))) ### THIS LINE IS WRONG
+                    else:
+                        unit.attack(closestEnemy)
+
+                    self._nodeState = State.FAILURE
+                    return State.FAILURE
+                # If we do not have stalkers, we cannot KITE  -> Go to next behaviour in the Sequence
+                else:
+                    self._nodeState = State.SUCCESS
+                    return State.SUCCESS
+
+
+class attackEnemy(BTNode):
+    def __init__(self, game_info, units):
+        self._game_info = game_info
+        self.units = units
+
+    def Evaluate(self, units):
+        enemy = self.on_enemy_unit_entered_vision()
+        for unit in self.units():
+            unit.attack(enemy)
+
+        return State.FAILURE
+
+#class fleeEnemy():
 
 
 
